1450c1a9fa10a792590084fc5aad561e14791bb1
rohan-z1 <rohan.zode@chat360.io>
2025-07-08 17:10:22 +0530

    changes in work experience

--- src/portfolio.js
+++ src/portfolio.js
@@ -161,29 +161,22 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: 'Software Engineer',
-      company: 'Facebook',
-      companylogo: require('./assets/images/facebookLogo.png'),
-      date: 'June 2018 – Present',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      descBullets: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      ],
+      role: 'Backend Developer',
+      company: 'Sumanela Technology Ventures Pvt. Ltd.',
+      companylogo: require('./assets/images/sumanel.png'),
+      date: 'Dec 2024 – Present',
     },
     {
-      role: 'Front-End Developer',
-      company: 'Quora',
-      companylogo: require('./assets/images/quoraLogo.png'),
-      date: 'May 2017 – May 2018',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      role: 'Web Developer',
+      company: 'Octarange Technologies Pvt. Ltd.',
+      companylogo: require('./assets/images/octa.png'),
+      date: 'Jan 2024 – Jun 2024',
     },
     {
-      role: 'Software Engineer Intern',
-      company: 'Airbnb',
-      companylogo: require('./assets/images/airbnbLogo.png'),
-      date: 'Jan 2015 – Sep 2015',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      role: 'Cloud Intern',
+      company: 'Cravita Technologies Pvt. Ltd.',
+      companylogo: require('./assets/images/images.jpeg'),
+      date: 'March 2023 – Aug 2023',
     },
   ],
 };
