1a6feb481e646ebc010c1ba786cc4353c3b303fc
rohan <rohan.zode@chat360.io>
2026-07-28 23:48:13 +0530

    new resume changes

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,9 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={greeting.resumeLink}
-                    target="_blank"
-                    rel="noreferrer"
+                    href={require("./resume.pdf")}
+                    download="Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
